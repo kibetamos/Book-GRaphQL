f62df6a4fe1e7059caf2a953150cf7b343742e3d
kibet kilel <amos.rotich@strathmore.edu>
2022-04-15 12:06:44 +0300

    Express app and create schema

--- server/app.js
+++ server/app.js
@@ -1,1 +1,15 @@
-const express = required("express");
+const express = require('express');
+const graphqlHTTP = require('express-graphql');
+
+const app = express();
+
+
+app.use('/graphql', graphqlHTTP({
+
+}));
+
+app.listen(4000, () =>{
+    console.log('Now Listening for requests')
+});
+
+
