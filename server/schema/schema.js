ac031e84c3fe5a51ef8295d65f381ffe75e8332c
kibet kilel <amos.rotich@strathmore.edu>
2022-04-15 15:32:24 +0300

    schema

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,6 +1,12 @@
 const graphql = require('graphql');
+const_ = require('lodash');
 
-const{GraphQLObjectType, GraphQLString} = graphql;
+const{GraphQLObjectType, GraphQLString, GraphQLSchema} = graphql;
+
+// lets add dummy data
+var book=[
+    {name:'name one', genre:'genrer1', id:'1'}
+];
 
 const BookType = new GraphQLObjectType({
     name:'Book',
@@ -10,4 +16,24 @@ const BookType = new GraphQLObjectType({
         genre:{type:GraphQLString}
     })
 
+});
+
+const RootQuery = new GraphQLObjectType({
+    name:'RootQueryType',
+    fields:{
+        book:{
+            type: BookType,
+            args:{id:{type:GraphQLString}},
+            resolve(parent, args){
+                // return Book.findById(args.id);
+                return _.find(books,{id:args.id})
+
+            }
+        }
+    }
+
+});
+
+module.exports = new GraphQLSchema({
+    query:RootQuery
 });
