d61ff0259ae4e7de7576d5928974978ff2706ec5
kibet kilel <amos.rotich@strathmore.edu>
2022-04-18 21:15:33 +0300

    Update

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import ApolloClient from 'apollo-boost';
 import { ApolloProvider } from 'react-apollo';
-
+import { version } from "graphql/index.mjs"; //
 // components
 import BookList from './components/BookList';
 
