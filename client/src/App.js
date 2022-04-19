baa586b52f31ed762abc692ef045200d703ff754
kibet kilel <amos.rotich@strathmore.edu>
2022-04-20 01:46:49 +0300

    Update

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
 import ApolloClient from 'apollo-boost';
 import { ApolloProvider } from 'react-apollo';
-import { version } from "graphql/index.mjs"; //
+
 // components
 import BookList from './components/BookList';
+import AddBook from './components/AddBook';
 
 // apollo client setup
 const client = new ApolloClient({
@@ -15,8 +16,9 @@ class App extends Component {
     return (
         <ApolloProvider client={client}>
             <div id="main">
-                <h1>Ninja's Reading List</h1>
+                <h1>Reading List</h1>
                 <BookList />
+                <AddBook />
             </div>
         </ApolloProvider>
     );
